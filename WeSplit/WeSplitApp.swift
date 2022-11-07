a7018f7ff984470edf7e0fc5ab95321852674840
Faizaan Khan <faizaankhan24@gmail.com>
2022-11-07 18:15:10 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Faizaan Khan on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
