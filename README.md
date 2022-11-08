7e1449447671b9d0ee8c414be8da360ce9290101
Faizaan Khan <faizaankhan24@gmail.com>
2022-11-08 15:26:18 -0500

    UI changes and ReadMe

---
+++ README.md
@@ -0,0 +1,7 @@
+# WeSplit
+
+## Project - 1 #100DaysOfSwift
+
+This project was built with the help of videos from the 100 days of Swift UI course. The course was built with SwiftUI.
+
+The project can split your restaurant bill in addition to adding the tip perecentage to the bill amount. 
--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmount = 0.0
+    @State private var numberOfpeople: Int = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    let tipPercentages = [10,15,20,25,0]
+    
+    var splitAmount: Double{
+        let peopleCount = Double(numberOfpeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = billAmount / 100 * tipSelection
+        let grandTotal = billAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var totalBillAmount: Double{
+        let tipSelection = Double(tipPercentage)
+        let tipValue = billAmount/100 * tipSelection
+        let totalAmount = billAmount + tipValue
+        
+        return totalAmount
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Enter the Amount", value: $billAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfpeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section{
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(0..<101){
+                            Text($0, format: .percent)
+                        }
+                    }
+//                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much percentage do you want to leave?")
+                }
+                
+                Section{
+                    Text(totalBillAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Total amount after tax")
+                }
+                
+                Section{
+                    Text(splitAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                } header: {
+                    Text("Amount per person")
+                }
+            }
+            .navigationTitle("We Split")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
